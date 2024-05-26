ff55aee729c20dac7b320273133f70ced2844cc4
22u208 <kam22u208@student.bmstu.ru>
2024-05-27 00:39:06 +0300

    Added lab_08

---
+++ lab_08_00/Lab_01/MyClasses.h
@@ -0,0 +1,25 @@
+#pragma once
+
+public ref class myPoint {
+public:
+	double X;
+	double Y;
+
+	myPoint(double x, double y)
+	{
+		X = x;
+		Y = y;
+	}
+
+	bool operator==(myPoint^ other)
+	{
+		return (this->X == other->X) && (this->Y == other->Y);
+	}
+};
+
+
+enum Wait {
+	wait,
+	no_wait,
+	error
+};
